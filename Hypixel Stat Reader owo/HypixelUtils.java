bc504412944d7d1e866c5737078eedf216567ce0
NoSkidSkies <78136681+SkiesPlusPlus@users.noreply.github.com>
2021-09-26 17:36:32 +0530

    Update HypixelUtils.java

--- Hypixel Stat Reader owo/HypixelUtils.java
+++ Hypixel Stat Reader owo/HypixelUtils.java
@@ -1,9 +1,7 @@
 public class GaypixelUtils{
   
-   private String hypixelRank = "owo"; 
+   private String hypixelRank = ""; 
    private static JSONObject jsonObject;
-   //u can prob use google gson
-  
    //https://api.slothpixel.me/api/players/{PlayerName}
   
    public static String getHypixelRank(String name, boolean formatted){
@@ -13,11 +11,5 @@ public class GaypixelUtils{
        }else{
             return jsonObject.getString("rank");
        }
-    }
-  
-  //Reading custom jsons are very easy u can prolly figure out urself ok bye
-  //ps : u can use github to host ur jsons too ok
-   
-  
-  
+    } 
 }
